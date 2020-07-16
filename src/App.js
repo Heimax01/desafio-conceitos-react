0446b68d31690e1a7fe2604fbd58bd0450138eb8
Heimax01 <41298893+Heimax01@users.noreply.github.com>
2020-07-16 16:46:56 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import "./styles.css";
 
-import api from './services/api'
-
+import { useState, useEffect } from 'react';
+import api from './services/api';
 
 function App() {
 
-  const [repositories, setRepositories] = useState([])
+  const [repositoriesList, setRepositories] = useState([]);
 
   useEffect(() => {
-
-    api.get('/repositories').then(response => {
-      setRepositories(response.data)
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
     })
-
   }, [])
 
   async function handleAddRepository() {
-    
-    const response = await api.post('/repositories', {
-      title: `Novo Repositório ${Date.now()}`,
-      owner: 'Heitor Cordeiro'
+    const repository = await api.post('repositories', {
+      title: "Repository",
+      url: 'http://github.com/thiagocdn',
+      techs: ["ReactJS"]
     })
 
-    const repository = response.data
-
-    setRepositories([...repositories, repository])
-
-
+    setRepositories([...repositoriesList, repository.data])
   }
 
   async function handleRemoveRepository(id) {
-    
-    
-    const response = await api.delete(`repositories/${id}`)
-    
-    
-    setRepositories([...repositories.filter(repository => repository.id !== id)])
+    await api.delete(`repositories/${id}`)
 
-
-
+    setRepositories(repositoriesList.filter(repository => repository.id !== id));
   }
+
 
   return (
     <div>
-    { repositories.map( repository => (
-      <ul key={repository.id} data-testid="repository-list">
-       <li>
-         {repository.title}
+      <ul data-testid="repository-list">
+        {repositoriesList.map(repository => 
+          <li key={repository.id}>
 
-         <button onClick={() => handleRemoveRepository(repository.id)}>
-           Remover
-         </button>
-       </li>
-     </ul>
-      ))
-    }
+            <ul>
+              <li><a href={repository.url} target="_blank">{repository.title}</a></li>
+              <li>Likes: {repository.likes}</li>
+            </ul>
+
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+
+          </li>
+          )}
+      </ul>
+
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
